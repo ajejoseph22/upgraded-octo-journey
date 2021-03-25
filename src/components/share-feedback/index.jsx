831c3dd51461a2c008c8922c9a27ffefa1d60e53
Joseph Aje <ajejoseph22@gmail.com>
2021-03-25 01:44:13 +0100

    chore: added api calls in share feedback page component

--- src/components/share-feedback/index.jsx
+++ src/components/share-feedback/index.jsx
@@ -1,11 +1,15 @@
 import styled from "styled-components";
-import { useEffect } from "react";
+import { useEffect, useState, useCallback } from "react";
+import { getUsers } from "../../services/users";
+import Spinner from "../spinner";
+import ErrorPage from "../error-page";
 
 const ShareFeedBackPageWrapper = styled.div`
   width: 600px;
   height: 60vh;
   margin: 10vh auto;
-  background: pink;
+  box-shadow: 0 0 4px rgba(0, 0, 0, 0.25);
+  overflow-x: auto;
 
   #share-feedback-page_top-area {
     display: flex;
@@ -19,7 +23,46 @@ const ShareFeedBackPageWrapper = styled.div`
 `;
 
 const ShareFeedBackPage = () => {
-  useEffect(() => {}, []);
+  const [users, setUsers] = useState([]);
+  const [isLoading, setisLoading] = useState(false);
+  const [hasError, setHasError] = useState(false);
+
+  const resetLoadingAndErrorState = () => {
+    setHasError(false);
+    setisLoading(true);
+  };
+
+  const fetchUsersAndSetState = useCallback(async () => {
+    try {
+      resetLoadingAndErrorState();
+
+      const userData = await getUsers();
+      setUsers(userData);
+
+      console.log(userData);
+    } catch (e) {
+      setHasError(true);
+    } finally {
+      setisLoading(false);
+    }
+  }, []);
+
+  useEffect(() => {
+    (async () => {
+      await fetchUsersAndSetState();
+    })();
+  }, [fetchUsersAndSetState]);
+
+  if (isLoading) return <Spinner color="#000" />;
+
+  if (hasError)
+    return (
+      <ErrorPage
+        retryFunction={fetchUsersAndSetState}
+        message="An error occured while fetching users"
+        color="#000"
+      />
+    );
 
   return (
     <ShareFeedBackPageWrapper>
@@ -27,7 +70,9 @@ const ShareFeedBackPage = () => {
         <header>Share Feedback</header>
         <span>Dropdown</span>
       </div>
-      <div id="share-feedback-page_content">content</div>
+      <div id="share-feedback-page_content">
+        {users.map((user) => user.firstName)}
+      </div>
     </ShareFeedBackPageWrapper>
   );
 };
