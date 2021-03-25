3e6de7b8a39a1521107a62e9a3d9293419f9adfb
Joseph Aje <ajejoseph22@gmail.com>
2021-03-25 23:37:06 +0100

    chore: set out share feedback page layout

--- src/components/share-feedback/index.jsx
+++ src/components/share-feedback/index.jsx
@@ -8,12 +8,41 @@ const ShareFeedBackPageWrapper = styled.div`
   width: 600px;
   height: 60vh;
   margin: 10vh auto;
-  box-shadow: 0 0 4px rgba(0, 0, 0, 0.25);
-  overflow-x: auto;
 
-  #share-feedback-page_top-area {
-    display: flex;
-    justify-content: space-between;
+  #share-feedback-page {
+    &_top-area {
+      display: flex;
+      justify-content: space-between;
+      margin-bottom: 10px;
+    }
+
+    &_content {
+      box-shadow: 0 0 4px rgba(0, 0, 0, 0.25);
+      height: 100%;
+      overflow-x: auto;
+
+      .feedback-item {
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+        padding: 10px 15px;
+      }
+
+      .profile-image {
+        border-radius: 50%;
+        margin-right: 20px;
+      }
+
+      .user-profile-area {
+        display: flex;
+        align-items: center;
+      }
+
+      button {
+        cursor: pointer;
+        padding: 5px 10px;
+      }
+    }
   }
 
   header {
@@ -71,7 +100,23 @@ const ShareFeedBackPage = () => {
         <span>Dropdown</span>
       </div>
       <div id="share-feedback-page_content">
-        {users.map((user) => user.firstName)}
+        {users.map(({ avatar, firstName, lastName }) => (
+          <div className="feedback-item">
+            <div className="user-profile-area">
+              <img
+                width={58}
+                height={58}
+                src={avatar}
+                alt={`${firstName}-profile`}
+                className="profile-image"
+              />
+              <span>
+                {firstName} {lastName}
+              </span>
+            </div>
+            <button>Fill</button>
+          </div>
+        ))}
       </div>
     </ShareFeedBackPageWrapper>
   );
