804845ac9d42cfe5f3dd107bd75a0c3c2e751878
Joseph Aje <ajejoseph22@gmail.com>
2021-03-26 22:18:28 +0100

    chore: added utility constants for form

--- src/util/constants.js
+++ src/util/constants.js
@@ -1,2 +1,13 @@
 export const apiUrl =
   "https://frontend-exercise-api.netlify.app/.netlify/functions/server";
+
+export const errorObj = {
+  getUsers: "An error occured while fetching users",
+  getQuestions: "An error occured while fetching questions",
+};
+
+export const questionTypes = {
+  scale: "scale",
+  multipleChoice: "multipleChoice",
+  text: "text",
+};
