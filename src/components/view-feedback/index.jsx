765f2612cf38c1fe7e9ed6949b10077921085abe
Joseph Aje <ajejoseph22@gmail.com>
2021-03-27 14:54:06 +0100

    chore: created view feedback component

--- src/components/view-feedback/index.jsx
+++ src/components/view-feedback/index.jsx
@@ -1,3 +1,28 @@
-const ViewFeedBackPage = () => <div>Hi</div>;
+import styled from "styled-components";
+import { feedbackKey } from "../../util/constants";
+import NoFeedback from "./no-feedback";
+import FeedBackDisplay from "./feedback-display";
+import { useContext } from "react";
+import { AppContext } from "../../App";
 
+const ViewFeedBackPageWrapper = styled.div`
+  padding: 50px 0;
+`;
+
+const ViewFeedBackPage = () => {
+  const storedFeedback = localStorage.getItem(feedbackKey);
+  const { users } = useContext(AppContext);
+
+  const isThereFeedBack =
+    storedFeedback &&
+    users.some((user) =>
+      Object.keys(JSON.parse(storedFeedback)).includes(user.id)
+    );
+
+  return (
+    <ViewFeedBackPageWrapper>
+      {isThereFeedBack ? <FeedBackDisplay /> : <NoFeedback />}
+    </ViewFeedBackPageWrapper>
+  );
+};
 export default ViewFeedBackPage;
